849c4408e91e6d3a7316f3360de1039ca3f27f77
yotsuyubi <yotsuyubi@mail.net>
2021-05-30 13:40:11 +0900

    update

--- music2vec/test.py
+++ music2vec/test.py
@@ -6,23 +6,49 @@ import torch as th
 import scipy.stats as stats
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
+import argparse
 
 
-model = Music2Vec()
-model.load_state_dict(
-    th.load('./model.pth')
-)
-model.eval()
 
-test_loader = DataLoader(
-    GT('download', download=True, subset='testing'), 
-    batch_size=8,
-    num_workers=4, shuffle=False
-)
+if __name__ == '__main__':
 
-train = Trainer()
-train.model = model
+    parser = argparse.ArgumentParser(
+        description='music2vec.test: test music2vec model.'
+    )
+    parser.add_argument(
+        'model_path', metavar='<model_path>', 
+        help='dir for saved model.'
+    )
+    parser.add_argument(
+        'dataset_root', metavar='<dataset_root>', 
+        help='root for dataset.'
+    )
+    parser.add_argument(
+        '-g', '--num_gpus', 
+        type=int, help='number of gpu use. to test using cpu, this must be 0. default is 0.', 
+        default=0
+    )
 
-trainer = pl.Trainer()
+    args = parser.parse_args()
 
-trainer.test(model=train, test_dataloaders=test_loader)
+    model = Music2Vec()
+    model.load_state_dict(
+        th.load(
+            args.model_path,
+            map_location='cpu' if args.num_gpus == 0 else 'cuda'
+        )
+    )
+    model.eval()
+
+    test_loader = DataLoader(
+        GT(args.dataset_root, download=True, subset='testing'), 
+        batch_size=8,
+        num_workers=4, shuffle=False
+    )
+
+    train = Trainer()
+    train.model = model
+
+    trainer = pl.Trainer()
+
+    trainer.test(model=train, test_dataloaders=test_loader)
